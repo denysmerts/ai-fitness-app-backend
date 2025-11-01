0302feba1d757a9c336573744ca880aa08d11fba
Denys Merts <denysmerts@Denyss-Mac-mini.local>
2025-11-01 15:35:03 +0000

    rename file

--- main.py
+++ main.py
@@ -88,5 +88,5 @@ def debug_features():
 # =========================
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
